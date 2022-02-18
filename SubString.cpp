f751889ee63714cc0d4c2b91920428a23e093956
AVHARISH96 <avharish96@gmail.com>
2022-02-18 19:10:27 +0530

    Modified SubString.cpp

--- SubString.cpp
+++ SubString.cpp
@@ -6,7 +6,15 @@ int main(){
 	string sub1="Hello Sala Ji";
 	int pos=sub1.find("Ji",0);
 	if(pos!=string::npos)
-	cout<<"Hello"<<pos;
+	cout<<"Hello"<<pos<<endl;
 	else
 	cout<<"String Not Found";
+	int pos1=sub1.find_first_not_of("Ji",0);
+    int pos2=sub1.find_last_not_of("Ji",0);
+    int pos3=sub1.find_first_of("Ji",0);
+    int pos4=sub1.find_last_of("Ji",0);
+    cout<<"sub1.find_first_not_of"<<pos1<<endl;
+    cout<<"find_last_not_of"<<pos2<<endl;
+     cout<<"find_first_of"<<pos3<<endl;
+    cout<<"find_last_of"<<pos4<<endl;
 }
